189ca33ed86a2c682f039c9e0849bf8cac9fda42
Sumant <sumanta.mishra@magicedtech.com>
2019-11-08 16:39:54 +0530

    first commit

--- src/config.js
+++ src/config.js
@@ -1,12 +1,12 @@
 module.exports = {
-    aws_table_name: 'es-lambda-dynamodb',
+    aws_table_name: 'movies-dynamodb-nodejs',
     aws_local_config: {
-      region: 'local',
-      endpoint: 'http://localhost:8000'
+        region: 'local',
+        endpoint: 'http://localhost:8000'
     },
     aws_remote_config: {
-      accessKeyId: 'ACCESS_KEY_ID',
-      secretAccessKey: 'SECRET_ACCESS_KEY',
-      region: 'us-east-1',
+        accessKeyId: 'ACCESS_KEY_ID',
+        secretAccessKey: 'SECRET_ACCESS_KEY',
+        region: 'us-east-1'
     }
 };
--- src/resources/movies/movies.router.js
+++ src/resources/movies/movies.router.js
@@ -6,5 +6,4 @@ export const moviesRouter = Router();
 
 moviesRouter.route('/').get(moviesController.getMovies);
 moviesRouter.route('/').post(moviesController.addMovie);
-moviesRouter.route('/search').get(moviesController.searchString);
 
